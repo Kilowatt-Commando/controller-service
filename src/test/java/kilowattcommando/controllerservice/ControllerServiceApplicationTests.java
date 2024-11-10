126a9b9499719b5cb560151e0c56d2da95a0b5af
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2024-11-10 17:36:01 +0100

    chore: Setup Spring Boot project
    Setup Spring Boot project with Kafka library

---
+++ src/test/java/kilowattcommando/controllerservice/ControllerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kilowattcommando.controllerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

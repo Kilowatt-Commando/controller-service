f8c10772fc09d89bd7cfe422602bd5562078529b
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2025-01-06 00:01:10 +0100

    fix(test): Added mocking to PowerplantRestAPITest

--- src/main/java/kilowattcommando/controllerservice/kafka/CommandSender.java
+++ src/main/java/kilowattcommando/controllerservice/kafka/CommandSender.java
@@ -3,6 +3,8 @@ package kilowattcommando.controllerservice.kafka;
 import dto.PowerplantCommand;
 import dto.PowerplantOperation;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CommandSender implements PowerPlantCommandSender {
+    private static final Logger log = LoggerFactory.getLogger(PowerPlantCommandSender.class);
+
     @Value("${kafka.controlTopic}")
     private String powerPlantControlTopic;
 
@@ -22,5 +26,6 @@ public class CommandSender implements PowerPlantCommandSender {
         ProducerRecord<String, PowerplantOperation> commandRecord = new ProducerRecord<>(powerPlantControlTopic, powerplantOperation);
         commandRecord.headers().add("targetConsumerId", powerPlantId.getBytes());
         kafkaTemplate.send(commandRecord);
+        log.info("Sent command {} to powerplant {}", command, powerPlantId);
     }
 }

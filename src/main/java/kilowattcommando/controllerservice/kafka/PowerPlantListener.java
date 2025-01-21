645aeb12bed2e163c07bb8ef12acf3eb0749cfd7
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2025-01-21 21:43:41 +0100

    feat: Add ActivityHandler to KafkaListener

--- src/main/java/kilowattcommando/controllerservice/kafka/PowerPlantListener.java
+++ src/main/java/kilowattcommando/controllerservice/kafka/PowerPlantListener.java
@@ -1,6 +1,7 @@
 package kilowattcommando.controllerservice.kafka;
 
 import dto.PowerplantStatus;
+import kilowattcommando.controllerservice.handlers.PowerPlantActivityHandler;
 import kilowattcommando.controllerservice.handlers.PowerPlantStatusHandler;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +12,18 @@ import org.springframework.stereotype.Service;
 public class PowerPlantListener {
 
     private final PowerPlantStatusHandler powerPlantStatusHandler;
+    private final PowerPlantActivityHandler powerPlantActivityHandler;
 
-    public PowerPlantListener(PowerPlantStatusHandler powerPlantStatusHandler) {
+    public PowerPlantListener(PowerPlantStatusHandler powerPlantStatusHandler, PowerPlantActivityHandler powerPlantActivityHandler) {
         this.powerPlantStatusHandler = powerPlantStatusHandler;
+        this.powerPlantActivityHandler = powerPlantActivityHandler;
     }
 
     @KafkaListener(topics = "${kafka.powerplant.topic}", groupId = "${kafka.groupId}")
     public void listen(ConsumerRecord<String, ?> message) {
             if(message.value() instanceof PowerplantStatus powerplantStatus) {
                 powerPlantStatusHandler.handle(powerplantStatus);
+                powerPlantActivityHandler.handle(powerplantStatus);
             }
     }
 }

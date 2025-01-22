46d35e647655aa9fa59cbaa1c2d6c7dac748d20d
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2025-01-22 21:16:11 +0100

    fix: Add CrossOrigin policy

--- src/main/java/kilowattcommando/controllerservice/rest/PowerplantRestController.java
+++ src/main/java/kilowattcommando/controllerservice/rest/PowerplantRestController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/powerplant/{id}")
 public class PowerplantRestController {

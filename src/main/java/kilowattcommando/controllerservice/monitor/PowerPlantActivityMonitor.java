5fa87eddab7a64e8d15b90a3a6b4bc93883eeee3
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2025-01-22 10:46:12 +0100

    feat: Added message broker for push notifications

--- src/main/java/kilowattcommando/controllerservice/monitor/PowerPlantActivityMonitor.java
+++ src/main/java/kilowattcommando/controllerservice/monitor/PowerPlantActivityMonitor.java
@@ -3,10 +3,13 @@ package kilowattcommando.controllerservice.monitor;
 import kilowattcommando.controllerservice.data.PowerPlantActivity;
 import kilowattcommando.controllerservice.data.PowerPlantActivityRepository;
 import kilowattcommando.controllerservice.handlers.PowerPlantLoggingHandler;
+import kilowattcommando.controllerservice.push.PushNotificationController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -14,19 +17,25 @@ import org.springframework.stereotype.Service;
 @Component
 public class PowerPlantActivityMonitor {
     private PowerPlantActivityRepository powerPlantActivityRepository;
+    private PushNotificationController pushNotificationController;
 
     private static final Logger log = LoggerFactory.getLogger(PowerPlantActivityMonitor.class);
 
-    public PowerPlantActivityMonitor(PowerPlantActivityRepository powerPlantActivityRepository) {
+    public PowerPlantActivityMonitor(PowerPlantActivityRepository powerPlantActivityRepository, PushNotificationController pushNotificationController) {
         this.powerPlantActivityRepository = powerPlantActivityRepository;
+        this.pushNotificationController = pushNotificationController;
     }
 
     @Scheduled(fixedDelay = 120_000L)
     public void checkForMissingPowerPlants() {
         Streamable<PowerPlantActivity> missingPowerplants = this.powerPlantActivityRepository.findMissingPowerPlants();
-        missingPowerplants.forEach(powerPlantActivity -> log.error("Missing powerplant detected: {}", powerPlantActivity.getName()));
+        missingPowerplants.forEach(powerPlantActivity -> {
+            log.error("Missing powerplant detected: {}", powerPlantActivity.getName());
+            pushNotificationController.sendMessage(String.format("Connection with powerplant %s is lost.", powerPlantActivity.getName()));
+        });
         if(missingPowerplants.isEmpty()) {
             log.info("No missing powerplant detected");
+            pushNotificationController.sendMessage("No missing powerplants");
         }
     }
 }
---
+++ src/main/java/kilowattcommando/controllerservice/push/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package kilowattcommando.controllerservice.push;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/message-broker");
+        registry.addEndpoint("/message-broker").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/powerplant");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
